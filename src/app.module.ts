9f5530778f0b74e1cfbbddea6c17153658a474a1
himalf <himal.fullel15@gmail.com>
2024-12-30 17:06:22 +0545

    feat: add imports items in main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,25 @@ import { ProductController } from './product/product.controller';
 import { ProductService } from './product/product.service';
 import { ProductModule } from './product/product.module';
 import { CategoryController } from './category/category.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product/product.entity';
+import { Category } from './category/category.entity';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [ProductModule],
-  controllers: [ProductController, CategoryController],
-  providers: [ProductService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Product, Category],
+      synchronize: true,
+    }),
+    ProductModule,
+    CategoryModule,
+  ],
 })
 export class AppModule {}
