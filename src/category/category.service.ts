9ada27a9c38f126a95758a34e738800032596ea4
himalf <himal.fullel15@gmail.com>
2024-12-30 16:35:46 +0545

    feat: create and get category in services

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(name: string): Promise<Category> {
+    const category = this.categoryRepository.create({ name });
+    return this.categoryRepository.save(category);
+  }
+  async findAll(): Promise<Category[]> {
+    return this.categoryRepository.find();
+  }
+  async findOne(id: number): Promise<Category> {
+    return this.categoryRepository.findOne({ where: { id } });
+  }
+}
