2651167385b6b9074ecf10959a1d511ed5978e05
himalf <himal.fullel15@gmail.com>
2024-12-30 16:54:44 +0545

    feat: add category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { Category } from './category.entity';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+  @Post()
+  async create(@Body() createCategoryDto: { name: string }): Promise<Category> {
+    return this.categoryService.create(createCategoryDto.name);
+  }
+
+  @Get()
+  async findAll(): Promise<Category[]> {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<Category> {
+    return this.categoryService.findOne(id);
+  }
+}
