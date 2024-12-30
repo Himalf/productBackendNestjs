f758501d7a95f60e98c7daa88d5fa78f4e4add42
himalf <himal.fullel15@gmail.com>
2024-12-30 16:29:40 +0545

    feat: add create and get services

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const product = this.productRepository.create(createProductDto);
+    return this.productRepository.save(product);
+  }
+  async findAll(): Promise<Product[]> {
+    return this.productRepository.find({ relations: ['category'] });
+  }
+  async findOne(id: number): Promise<Product> {
+    return this.productRepository.findOne({
+      where: { id },
+      relations: ['category'],
+    });
+  }
+}
