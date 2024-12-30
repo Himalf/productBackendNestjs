6eefd8d52193d7212fa552cd82b462a5f1dd62f0
himalf <himal.fullel15@gmail.com>
2024-12-30 16:48:43 +0545

    feat: add product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './product.entity';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  async create(@Body() createProductDto: CreateProductDto): Promise<Product> {
+    return this.productService.create(createProductDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Product[]> {
+    return this.productService.findAll();
+  }
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<Product> {
+    return this.productService.findOne(id);
+  }
+}
